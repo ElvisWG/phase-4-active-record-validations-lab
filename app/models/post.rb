45056abc8afc864477cd430461632c7dafe36d4d
Elvis Wanjiru <elvis.gatukui@student.moringaschool.com>
2022-11-27 14:17:25 +0300

    Done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
-end
+    validates :title, exclusion: { in: ["True Facts"] }
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validates :title, presence: true
+  end
